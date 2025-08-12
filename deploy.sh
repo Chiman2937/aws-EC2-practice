ed50d1b9983ca06e051e6e622991c2609d1a69b0
Chiman2937 <cyga2933@gmail.com>
2025-08-12 17:46:06 +0900

    fix: deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ sudo npm install
 sudo npm run build
 pm2 describe next_app >/dev/null 2>&1 \
   && pm2 restart next_app \
-  || pm2 start npm --name next_app -- start --cwd /home/ubuntu/aws-EC2-practice -- -H 0.0.0.0 -p 3000
+  || pm2 start npm --name next_app --cwd /home/ubuntu/aws-EC2-practice -- start -- -H 0.0.0.0 -p 3000
