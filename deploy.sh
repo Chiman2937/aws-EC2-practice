0088df4de10cf18fa12f5039a924ba59dd4622ad
Chiman2937 <cyga2933@gmail.com>
2025-08-12 16:09:17 +0900

    fix: deploy.sh에서 PM2가 없을 때 분기처리

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ cd /home/ubuntu/aws-EC2-practice
 git pull origin main
 sudo npm install
 sudo npm run build
-pm2 restart next_app
+pm2 describe next_app >/dev/null 2>&1 && pm2 restart next_app || pm2 start "npm start" --name next_app
